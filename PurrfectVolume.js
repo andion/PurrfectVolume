ce004a98b0ac682af1dd2f43a8a1c76eac32c43a
Lucas Andión <lucas.andion@trabesoluciones.com>
2020-11-16 12:34:08 +0100

    Headphone detection

--- PurrfectVolume.js
+++ PurrfectVolume.js
@@ -1,9 +1,12 @@
 class Volimiter {
-  constructor(appName, maxVolume) {
+  constructor(appName, maxVolume, onlyOnHeadphones) {
     this.app = Application.currentApplication();
     this.app.includeStandardAdditions = true;
     this.appName = appName;
     this.maxVolume = maxVolume;
+    this.onlyOnHeadphones = onlyOnHeadphones;
+    this.headPhonesConnected = !this.onSpeaker();
+    this.previousVolume = null;
   }
 
   get currentVolume() {
@@ -11,28 +14,72 @@ class Volimiter {
     return outputVolume;
   }
 
-  limitVolume() {
-		if(this.currentVolume > this.maxVolume) {
-			this.app.beep();
-	    this.app.setVolume(null, { outputVolume: this.maxVolume });
-		}
+  get outputTypeText() {
+    return this.onlyOnHeadphones ? "🎧 headphones" : "🔊 global";
   }
 
-  showNotification() {
+  get volumeLimitText() {
+    return `Limiting your ${this.outputTypeText} to ${this.maxVolume}% to protect your ears`;
+  }
+
+  get shouldLimitVolume() {
+    return (
+      this.onlyOnHeadphones &&
+      this.headPhonesConnected &&
+      this.currentVolume > this.maxVolume
+    );
+  }
+
+  onSpeaker() {
+    const audio = this.app.doShellScript("system_profiler SPAudioDataType");
+    return /Default Output Device: Yes(?:(?!Output Source: )[\s\S])*Output Source: Internal Speakers(?:.*)$/gm.test(
+      audio
+    );
+  }
+
+  checkHeadPhones() {
+    if (this.onlyOnHeadphones) {
+      const areHeadphonesConnected = !this.onSpeaker();
+      const wereHeadphonesConnected = this.headPhonesConnected;
+      if (areHeadphonesConnected !== wereHeadphonesConnected) {
+        if (areHeadphonesConnected) {
+          this.headPhonesConnected = true;
+          this.limitVolume();
+          this.app.displayNotification("", {
+            withTitle: "🎧 Headphones connected",
+            subtitle: this.volumeLimitText,
+          });
+        } else {
+          this.headPhonesConnected = false;
+          this.app.displayNotification("", {
+            withTitle: "🔇 Headphones disconnected",
+            subtitle: "Stopping volume limit, be careful",
+          });
+        }
+      }
+    }
+  }
+
+  limitVolume() {
+    if (this.shouldLimitVolume) {
+      this.app.beep();
+      this.app.setVolume(null, { outputVolume: this.maxVolume });
+    }
+  }
+
+  startNotification() {
     this.app.displayNotification("", {
       withTitle: this.appName,
-      subtitle: `Limiting your 🎧 volume to ${
-        this.maxVolume
-      }% to protect your ears`
+      subtitle: this.volumeLimitText,
     });
   }
 }
 
-const PurrfectVolume = new Volimiter("Purrfect volume 😸", 25);
-PurrfectVolume.showNotification();
+const PurrfectVolume = new Volimiter("Purrfect volume2 😸", 20, true);
+PurrfectVolume.startNotification();
 
 function idle() {
-	PurrfectVolume.limitVolume();
+  PurrfectVolume.checkHeadPhones();
+  PurrfectVolume.limitVolume();
   return 0.5;
 }
-
--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-## Purrfect Volume App
+## Purrfect Volume 2 App
 
-Keeps your ears safe limiting the maximum volume for your Mac
+Keeps your ears safe limiting the maximum volume for your Mac. Now with Headphone detection.
 
 How to use it / customize it and more information in this [blog post](https://medium.com/trabe/limiting-your-macs-volume-in-2019-f314e20408ab)
 
-## Download a .zip with the app
+How to detect Headphones in this [2020 Update](https://medium.com/p/8623f99ec69f/)
+
+## Download a .zip with the updated app
 
 https://github.com/andion/PurrfectVolume/archive/master.zip
